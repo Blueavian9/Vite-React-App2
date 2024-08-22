c79dbc35c50f2bf5024c564d4522dd5db1b08e4c
Cesar Aguilar <Blueavian9@gmail.com>
2024-08-21 18:28:29 -0700

    main

--- All-String-Methods/script.js
+++ All-String-Methods/script.js
@@ -3,16 +3,13 @@ const str0 = "A String primitive";
 
 const str1 = new String("A String object");
 
-
-
 // String Properties > length
-const str2 = "Code Explained";    
+const str2 = "Code Explained";
 str2.length = 14;
 
-
 //String Character Access
-const str3 = [" C o d e   E x p l a i n  e  d "]
-indeces =    [  0 1 2 3 4 5 6 7 8 9 1 0 11 12
+const str3 = [" C | o | d | e |  | E | x | p | l | a | i  |  n |  e | d "];
+// indeces = [  0 | 1 | 2 | 3 |4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 ]
 str[0];
 str[5];
-str[str.length -1];
+str[str.length - 1];
